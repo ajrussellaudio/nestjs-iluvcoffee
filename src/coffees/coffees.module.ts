520fd61523c9839ce57c92d1b7867ad9f2ba61b0
Alan Russell <alan.russell@digirati.com>
2021-11-05 09:28:58 +0000

    Add async coffee brands factory

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from '../events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
@@ -11,7 +12,15 @@ import { Flavour } from './entities/flavour.entity';
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    { provide: 'COFFEE_BRANDS', useValue: ['buddy brew', 'nescafe'] },
+    {
+      provide: COFFEE_BRANDS,
+      // useFactory: async (connection: Connection): Promise<string[]> => {
+      useFactory: async (): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        return coffeeBrands;
+      },
+    },
   ],
   exports: [CoffeesService],
 })
